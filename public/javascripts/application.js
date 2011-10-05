ff581d1fd3c954e0a44cf3a9843d9624e8de8c44
sralston <sralston@gmail.com>
2011-10-04 20:09:19 -0700

    major rework of front page and js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,15 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+	jQuery.inputHighlight = function(color) {
+		$(".login_input").each(function() {
+			$(this).focus(function() {
+				$(this).css({"background" : color});
+			});
+			$(this).blur(function() {
+				$(this).css({"background" : "#c4c4c4"});
+			});
+		});
+	}
+	
+	
